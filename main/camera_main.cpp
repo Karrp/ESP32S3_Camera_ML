f88d9c3800f30b44de2c10b901f7e24491832573
Adam Karpinski <kogut0000@gmail.com>
2024-02-21 17:49:33 +0100

    Use XIAO Sense module

--- main/camera_main.cpp
+++ main/camera_main.cpp
@@ -22,7 +22,8 @@ extern "C"{
 // ----------- CAMERA -----------
 #include "esp_camera.h"
 
-#define BOARD_ESP_S3_EYE
+// #define BOARD_ESP_S3_EYE
+#define BOARD_XIAO_SENSE_ESP32S3
 
 // ESP32S3 EYE
 #ifdef BOARD_ESP_S3_EYE
@@ -46,6 +47,27 @@ extern "C"{
     #define CAM_PIN_D5 18
     #define CAM_PIN_D6 17
     #define CAM_PIN_D7 16
+
+#endif
+
+#ifdef BOARD_XIAO_SENSE_ESP32S3
+    #define CAM_PIN_PWDN     -1
+    #define CAM_PIN_RESET    -1
+    #define CAM_PIN_XCLK     10
+    #define CAM_PIN_SIOD     40
+    #define CAM_PIN_SIOC     39
+
+    #define CAM_PIN_D7       48
+    #define CAM_PIN_D6       11
+    #define CAM_PIN_D5       12
+    #define CAM_PIN_D4       14
+    #define CAM_PIN_D3       16
+    #define CAM_PIN_D2       18
+    #define CAM_PIN_D1       17
+    #define CAM_PIN_D0       15
+    #define CAM_PIN_VSYNC    38
+    #define CAM_PIN_HREF     47
+    #define CAM_PIN_PCLK     13
 
 #endif
 
